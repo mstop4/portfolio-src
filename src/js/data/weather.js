09a86b1b3a4527c0efbd06a4d9966f9379190fc6
mstop4 <mstop4@gmail.com>
2018-11-14 21:32:22 -0500

    modified weather sentence generator

--- src/js/data/weather.js
+++ src/js/data/weather.js
@@ -1,10 +1,23 @@
 module.exports = {
   'location': 'Toronto, Canada',
-  'prefixes': [
-    {
-      'prefix': 'a ',
-      'patterns': [RegExp('^2', 'g'), RegExp('781', 'g')]
-    }
+
+  'weatherConditions': [
+    { 'descriptor': 'stormy', 'pattern': RegExp('^2', 'g') },
+    { 'descriptor': 'drizzling', 'pattern': RegExp('^3', 'g') },
+    { 'descriptor': 'rainy', 'pattern': RegExp('^5', 'g') },
+    { 'descriptor': 'snowy', 'pattern': RegExp('^6', 'g') },
+    { 'descriptor': 'misty', 'pattern': RegExp('^701', 'g') },
+    { 'descriptor': 'smoky', 'pattern': RegExp('^711', 'g') },
+    { 'descriptor': 'hazy', 'pattern': RegExp('^721', 'g') },
+    { 'descriptor': 'sandstormy(?!)', 'pattern': RegExp('^731', 'g') },
+    { 'descriptor': 'foggy', 'pattern': RegExp('^741', 'g') },
+    { 'descriptor': 'sandy', 'pattern': RegExp('^751', 'g') },
+    { 'descriptor': 'dusty', 'pattern': RegExp('^761', 'g') },
+    { 'descriptor': 'volcanic(?!)', 'pattern': RegExp('^762', 'g') },    
+    { 'descriptor': 'blustery', 'pattern': RegExp('^771', 'g') },   
+    { 'descriptor': 'tornado(!)', 'pattern': RegExp('^781', 'g') },   
+    { 'descriptor': 'cloudy', 'pattern': RegExp('^80', 'g') },
+    { 'descriptor': 'clear', 'pattern': RegExp('^800', 'g') }
   ],
 
   'temperatureRanges': [
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,11 +1,10 @@
 const { openWeatherApiKey }  = require('./data/env');
-const { location, prefixes, temperatureRanges } = require('./data/weather');
+const { location, weatherConditions, temperatureRanges } = require('./data/weather');
 
 const getWeather = () => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${openWeatherApiKey}&units=metric`)
     .then((result) => result.json())
     .then((json) => {
-      // console.dir(json);
       console.log(evaluateWeather(json))
   });
 }
@@ -13,27 +12,32 @@ const getWeather = () => {
 const evaluateWeather = (weatherObj) => {
   const weatherIdStr = String(weatherObj.weather[0].id);
   const temp = weatherObj.main.temp;
-  let output = '(where it\'s a ';
+  let output = 'where it\'s a ';
 
+  // Temperature
   for (let i = 0; i < temperatureRanges.length; i++) {
     if (temp < temperatureRanges[i].maxTemp) {
-      output += `${temperatureRanges[i].descriptor} day and currently experiencing `;
+      output += `${temperatureRanges[i].descriptor} and `;
       break;
     }
   }
 
-  for (let i = 0; i < prefixes.length; i++) {
-    const prefix = prefixes[i];
-
-    for (let j = 0; j < prefix.patterns.length; j++) {
-      if (prefix.patterns[j].test(weatherIdStr)) {
-        output += prefix.prefix;
-        break;
-      }
+  // Weather
+  for (let i = 0; i < weatherConditions.length; i++) {
+    if (weatherConditions[i].pattern.test(weatherIdStr)) {
+      output += weatherConditions[i].descriptor;
+      break;
     }
   }
 
-  output += `${weatherObj.weather[0].description})`;
+  // Time of Day
+  const now = Date.now();
+  if (now >= weatherObj.sys.sunrise && now < weatherObj.sys.sunset) {
+    output += ' day';
+  } else {
+    output += ' night';
+  }
+
   return output;
 }
 
