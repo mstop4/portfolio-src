fb4d9e147608584be32fce3308161f761fa0f75e
M.S.T.O.P <mstop4@gmail.com>
2023-03-10 16:48:59 -0500

    update thumbnails

--- src/js/data/info.js
+++ src/js/data/info.js
@@ -16,7 +16,7 @@ const projects = [
   {
     'id': 1,
     'title': 'Cookie Land',
-    'previewStatic': 'img/gml-script-wizard.jpg',
+    'previewStatic': 'img/cookie-land.jpg',
     'previewAnim': 'vid/preview/gml-script-wizard.mp4',
     'fullAnim': 'vid/gml-script-wizard.mp4',
     'description': 'A free-to-play match-3 puzzle game on Facebook by Softgames.  Players complete levels and compete with friends to see who can reach the highest level, with new levels updated every week. Other features include daily challenge, a weekly Treasure Hunt events, and seasonal events where player can earn rewards.',
